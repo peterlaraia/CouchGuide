336299b3ce5f38425cff3c74bdd8f9586d1de55f
Peter Laraia <PeterLaraiaGit@gmail.com>
2017-07-10 16:42:57 -0700

    apply media query directive to search results

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -4,18 +4,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { EffectsModule } from '@ngrx/effects';
+import { ResultsComponent } from './results/results.component';
 import { SearchComponent } from './search.component';
 import { searchRoutes } from './search.routes';
 import { SearchService } from './search.service';
+import { SharedModule } from "../shared/shared.module";
 import { SearchEffects } from './store/search-effects';
-import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   imports: [
     CommonModule,
     EffectsModule.run(SearchEffects),
     ReactiveFormsModule,
-    RouterModule.forChild(searchRoutes)
+    RouterModule.forChild(searchRoutes),
+    SharedModule
   ],
   declarations: [SearchComponent, ResultsComponent],
   providers: [SearchService],
