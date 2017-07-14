4ed343e4f7404820150a5083ab20d5aad7afa66e
Peter Laraia <PeterLaraiaGit@gmail.com>
2017-07-14 14:47:05 -0700

    indicate home page as WIP, w/ button to search. style footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cg-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  redirectToSearch(): void {
+    this.router.navigate(['/search']);
   }
 
 }
